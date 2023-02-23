aa99f10cdb96c075a08d4ffbcbf73e4be785d8fc
Jason Harris <sirmanlypowers@gmail.com>
2023-02-23 10:22:28 -0800

    Tweaks to delay routines.

--- src/soc/microchip/lib/delay.c
+++ src/soc/microchip/lib/delay.c
@@ -10,11 +10,12 @@ Polled Delays
 
 //-----------------------------------------------------------------------------
 
+// global 1ms tick counter updated by SysTick_Handler.
 volatile uint32_t globalTick;
 
 //-----------------------------------------------------------------------------
 
-#define TICK_CLK 4		// systick count down clock in MHz
+#define TICK_CLK 4		// systick clock in MHz
 
 static uint64_t get_current_usecs(void) {
 	return (getTick() * 1000) + 1000 - (SysTick->VAL / TICK_CLK);
@@ -22,19 +23,23 @@ static uint64_t get_current_usecs(void) {
 
 //-----------------------------------------------------------------------------
 
+// millisecond delay
 void msDelay(uint32_t msecs) {
 	uint32_t start = getTick();
 	uint32_t wait = msecs;
-	// Add a period to guarantee minimum wait
+	// actual delay is between msecs and msecs+1 milliseconds.
 	if (wait < UINT32_MAX) {
 		wait++;
 	}
+	// delay (wrap around safe)
 	while ((getTick() - start) < wait) ;
 }
 
+// microsecond delay
 void usDelay(uint32_t usecs) {
-	uint64_t timeout = get_current_usecs() + usecs;
-	while (get_current_usecs() < timeout) ;
+	uint64_t start = get_current_usecs();
+	// delay (wrap around safe)
+	while ((get_current_usecs() - start) < usecs) ;
 }
 
 //-----------------------------------------------------------------------------
