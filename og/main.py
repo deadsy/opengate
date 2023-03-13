4d334571d01841b262c47a2933bc3bf3fbe00e3a
Jason Harris <sirmanlypowers@gmail.com>
2023-03-13 10:51:25 -0700

    Added a lambda for the scoller puts routine.

--- og/main.py
+++ og/main.py
@@ -6,18 +6,9 @@ import utime as time
 
 lcd = hd44780.LCD(4, (10, 11, 6, 7, 8, 9), 2, 16)
 
-
-def puts0(s):
-    lcd.puts(0, 0, s)
-
-
-def puts1(s):
-    lcd.puts(1, 0, s)
-
-
 def main():
-    s0 = scroll.scroll("Arbeit Macht Frei...", 16, puts0)
-    s1 = scroll.scroll("Arby's Makes Fries...", 16, puts1)
+    s0 = scroll.scroll("Arbeit Macht Frei...", 16, lambda s: lcd.puts(0, 0, s))
+    s1 = scroll.scroll("Arby's Makes Fries...", 16, lambda s: lcd.puts(1, 0, s))
     while True:
         s0.update()
         s1.update()
