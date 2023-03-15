57190f1799263944678fa9f0330a0afce2219ea0
Jason Harris <sirmanlypowers@gmail.com>
2023-03-15 10:51:24 -0700

    Adding initial modem code.

--- og/main.py
+++ og/main.py
@@ -17,19 +17,21 @@ r1 = relay.relay("r1", 16)
 
 # sim7600 modem
 uart = UART(0)
-uart.init(baudrate=115200)  # tx=0, rx=1
+uart.init(baudrate=115200, timeout=20)  # tx=0, rx=1
 modem = sim7600.modem(uart, pwr=11, fc=6)
-print("%s" % modem)
 
 
 def main():
-    lcd.symbol(0, (0, 0x0A, 0x1F, 0x1F, 0xE, 0x04, 0, 0))
-    s0 = scroll.scroll("\x00Arbeit Macht Frei...", 16, lambda s: lcd.puts(0, 0, s))
-    s1 = scroll.scroll("\x00\x00\x00Arby's Makes Fries...", 16, lambda s: lcd.puts(1, 0, s))
-    while True:
-        s0.update()
-        s1.update()
-        time.sleep_ms(400)
+    modem.start()
+    print(modem)
+
+    # lcd.symbol(0, (0, 0x0A, 0x1F, 0x1F, 0xE, 0x04, 0, 0))
+    # s0 = scroll.scroll("\x00Arbeit Macht Frei...", 16, lambda s: lcd.puts(0, 0, s))
+    # s1 = scroll.scroll("\x00\x00\x00Arby's Makes Fries...", 16, lambda s: lcd.puts(1, 0, s))
+    # while True:
+    #    s0.update()
+    #    s1.update()
+    #    time.sleep_ms(400)
 
 
 main()
--- og/sim7600.py
+++ og/sim7600.py
@@ -1,6 +1,20 @@
 # sim7600 driver
 
+import utime as time
 from machine import Pin
+
+
+class modem_error(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+        super().__init__(self.msg)
+
+
+def ok_check(s):
+    if s is None:
+        raise modem_error("response is None")
+    if not s.endswith("OK\r\n"):
+        raise modem_error("no ok in reponse")
 
 
 class modem:
@@ -8,6 +22,7 @@ class modem:
         self.uart = uart
         self.pwr_io = Pin(pwr, Pin.OUT)
         self.fc_io = Pin(fc, Pin.OUT)
+        self.info = {}
 
     def flight_control(self, state):
         self.fc_io.value(state)
@@ -15,10 +30,42 @@ class modem:
     def power(self, state):
         self.pwr_io.value(state)
 
-    def send_cmd(self, cmd):
-        self.serial.write("AT%s\r" % cmd)
+    def cmd(self, cmd):
+        self.uart.write("AT" + cmd + "\r")
+        rsp = b""
+        while True:
+            rx = self.uart.read()
+            if rx is not None:
+                rsp += rx
+                if rsp.endswith("OK\r\n"):
+                    break
+        return rsp
+
+    def set_echo(self, state):
+        rsp = self.cmd(("E0", "E1")[state])
+        ok_check(rsp)
+
+    def get_info(self):
+        rsp = self.cmd("I")
+        ok_check(rsp)
+        for s in rsp.split(b"\r\n"):
+            x = s.split(b":")
+            if len(x) == 2:
+                t = x[0].strip()
+                v = x[1].strip()
+                self.info[t.decode("utf-8")] = v.decode("utf-8")
+
+    def start(self):
+        self.flight_control(False)
+        self.power(False)
+        time.sleep_ms(100)
+        self.power(True)
+        time.sleep_ms(100)
+        self.set_echo(False)
+        self.get_info()
 
     def __str__(self):
         s = []
-        s.append("%s" % self.uart)
+        s.append(str(self.uart))
+        s.append(str(self.info))
         return "\n".join(s)
