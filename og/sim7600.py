ab867983d4d718a96004dd5d2b561122a024ac2f
Jason Harris <sirmanlypowers@gmail.com>
2023-03-16 10:55:22 -0700

    More modem queries. Getting SIM card info.

--- og/sim7600.py
+++ og/sim7600.py
@@ -10,19 +10,23 @@ class modem_error(Exception):
         super().__init__(self.msg)
 
 
-def ok_check(s):
-    if s is None:
-        raise modem_error("response is None")
-    if not s.endswith("OK\r\n"):
-        raise modem_error("no ok in reponse")
+def check_response(s):
+    if s is None or len(s) == 0:
+        raise modem_error("no response")
+    if "+CME ERROR" in s:
+        x = s.strip().split(b":")
+        msg = x[1].strip().decode("utf-8")
+        raise modem_error(msg)
+    if s.endswith("ERROR\r\n"):
+        raise modem_error("error")
 
 
 class modem:
-    def __init__(self, uart, pwr, fc):
+    def __init__(self, uart, pwr_pin, fc_pin):
         self.uart = uart
-        self.pwr_io = Pin(pwr, Pin.OUT)
-        self.fc_io = Pin(fc, Pin.OUT)
-        self.info = {}
+        self.pwr_io = Pin(pwr_pin, Pin.OUT)
+        self.fc_io = Pin(fc_pin, Pin.OUT)
+        self.info = None
 
     def flight_control(self, state):
         self.fc_io.value(state)
@@ -31,41 +35,73 @@ class modem:
         self.pwr_io.value(state)
 
     def cmd(self, cmd):
-        self.uart.write("AT" + cmd + "\r")
+        self.uart.write("AT%s\r" % cmd)
         rsp = b""
         while True:
             rx = self.uart.read()
             if rx is not None:
                 rsp += rx
-                if rsp.endswith("OK\r\n"):
+                if rsp.endswith(b"OK\r\n"):
+                    break
+                if rsp.endswith("\r\n") and "ERROR" in rsp:
                     break
         return rsp
 
-    def set_echo(self, state):
+    def set_echo(self, state):  # set the command echo
         rsp = self.cmd(("E0", "E1")[state])
-        ok_check(rsp)
+        check_response(rsp)
 
-    def get_info(self):
+    def get_info(self):  # get modem information
         rsp = self.cmd("I")
-        ok_check(rsp)
+        check_response(rsp)
+        info = {}
         for s in rsp.split(b"\r\n"):
             x = s.split(b":")
             if len(x) == 2:
                 t = x[0].strip()
                 v = x[1].strip()
-                self.info[t.decode("utf-8")] = v.decode("utf-8")
+                info[t.decode("utf-8")] = v.decode("utf-8")
+        return info
 
-    def start(self):
+    def set_cmee(self, mode):  # set the error code return type
+        rsp = self.cmd("+CMEE=%d" % mode)
+        check_response(rsp)
+
+    def get_iccid(self):  # get the sim card ICCID
+        rsp = self.cmd("+CICCID")
+        check_response(rsp)
+        x = rsp.strip().split(b"\r\n")[0]
+        x = x.split(b":")[1].strip()
+        return x.decode("utf-8")
+
+    def get_imsi(self):  # get the sim card IMSI
+        rsp = self.cmd("+CIMI")
+        check_response(rsp)
+        x = rsp.strip().split(b"\r\n")[0]
+        return x.decode("utf-8")
+
+    def get_sq(self):  # get signal quality (rssi, ber)
+        rsp = self.cmd("+CSQ")
+        check_response(rsp)
+        x = rsp.strip().split(b"\r\n")[0]
+        x = x.split(b":")[1].strip().split(b",")
+        assert len(x) == 2, "bad sq decode"
+        return (int(x[0]), int(x[1]))
+
+    def setup(self):  # initial modem setup
         self.flight_control(False)
         self.power(False)
         time.sleep_ms(100)
         self.power(True)
         time.sleep_ms(100)
-        self.set_echo(False)
-        self.get_info()
+        self.set_echo(False)  # echo off
+        self.set_cmee(2)  # string error codes
+        self.info = self.get_info()
+        self.info["ICCID"] = self.get_iccid()
+        self.info["IMSI"] = self.get_imsi()
 
     def __str__(self):
         s = []
-        s.append(str(self.uart))
-        s.append(str(self.info))
+        for k, v in self.info.items():
+            s.append("%s: %s" % (k, v))
         return "\n".join(s)
